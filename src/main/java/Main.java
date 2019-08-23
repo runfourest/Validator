c73ce808e0b084ecacd6a2075335c804e1016388
alexander fournier <aefournier50@gmail.com>
2019-08-23 14:05:47 -0400

    Unzipper Works- Write comparator and data structures next

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,22 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
-}
+    public static void main(String[] args){
+
+
+        /*
+
+        Todo: Create FileReaders and datastructures and write a comparator 
+         */
+
+
+        File f = new File("C:\\Users\\afournier\\IdeaProjects\\valfilechecker\\src\\main\\resources\\destination\\test");
+        ArrayList<File> files = new ArrayList<File>(Arrays.asList(f.listFiles()));
+        ArrayList<String> fileStringNames = new ArrayList<String>(Arrays.asList(f.list()));
+
+
+
+
+}}
--- src/test/java/UnzipTest.java
+++ src/test/java/UnzipTest.java
@@ -2,8 +2,8 @@ import util.Unzipper;
 
 public class UnzipTest {
     public static void main(String[] args){
-        String zipFilePath = "";
-        String destDirectory= "";
+        String zipFilePath = "C:\\Users\\afournier\\IdeaProjects\\valfilechecker\\src\\main\\resources\\test.zip";
+        String destDirectory= "C:\\Users\\afournier\\IdeaProjects\\valfilechecker\\src\\main\\resources\\destination";
         Unzipper unzipper = new Unzipper();
         try{
             unzipper.unZip(zipFilePath,destDirectory);
