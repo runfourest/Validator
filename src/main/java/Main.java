bb4f7fce78abea973bcc6e7f4c7ed34cf0939cd0
alexander fournier <aefournier50@gmail.com>
2019-08-26 13:54:57 -0400

    Node Object

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
+import models.Node;
+
 import java.io.*;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,6 +14,11 @@ public class Main {
 
 
         String filepath = "C:\\Users\\afournier\\IdeaProjects\\valfilechecker\\src\\main\\resources\\node.csv";
+
+
+
+
+
 
         Map<String,String> mapper = getMapFromNodeCSV(filepath);
         //mapper.keySet().stream().
@@ -34,20 +42,37 @@ public class Main {
         }
 
 
+        public static ArrayList<Integer> lambdaTest(int num1,int num2) {
+            ArrayList<Integer> arrayList  = new ArrayList<Integer>();
+            arrayList.add(num1);
+            arrayList.add(num2);
+            //List<Integer> odd = arrayList.stream().map(s -> Integer.valueOf(s))
+                    //.filter(number -)
+            return arrayList;
+        }
+
+
     /*Create Node.CSV File
     * Issue is with the .collect method only picking up a single column
-    * 
+    *
     * */
         public static Map<String,String> getMapFromNodeCSV(final String filepath)throws IOException {
 
             Stream<String> lines = Files.lines(Paths.get(filepath));
             Map<String,String> resultMap = lines.map(line -> line.split(","))
-                    .collect(Collectors.toMap(line -> line[0], line -> line[1]));
+                    .collect(Collectors.toMap(line -> line[0], line -> line[3]));
             lines.close();
             return resultMap;
         }
 
+        /*
+        public static Map<String,String> getMapFromObjectsCSV(final String objectsFilePath) throws IOException {
 
+            return
+
+        }
+
+*/
 }
 
 /*Create a method to read the objects csv ... WAY more complicated logic -.-*/
---
+++ src/main/java/models/Node.java
@@ -0,0 +1,23 @@
+package models;
+
+public class Node {
+
+    private String objectid;
+    private String parentid;
+    private String objectname;
+    private String someotherobjectid;
+
+    //crappy constructor 
+    public Node(String objectid, String parentid, String objectname, String someotherobjectid){
+
+        this.objectid = objectid;
+        this.parentid = parentid;
+        this.objectname = objectname;
+        this.someotherobjectid = someotherobjectid;
+
+    }
+
+
+
+    //getters and settersS
+}
