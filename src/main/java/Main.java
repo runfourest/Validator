cf722f31b5f2762175b0fe15cc1ee807e6b20dca
alexander fournier <aefournier50@gmail.com>
2019-08-30 16:43:15 -0400

    Filtering List Prior

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,40 @@
+import com.opencsv.CSVWriter;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
 import models.NodeCsv;
 import models.Objects;
 import models.UnifiedNode;
 import models.UnifiedNodeCollection;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
+    //private static final String nodeFilePath = "C:\\Users\\afournier\\IdeaProjects\\valfilechecker\\src\\main\\resources\\node.csv";
+    //private static final String objectFilePath = "C:\\Users\\afournier\\IdeaProjects\\valfilechecker\\src\\main\\resources\\objects.csv";
     private static final String nodeFilePath = "C:\\Users\\afournier\\IdeaProjects\\valfilechecker\\src\\main\\resources\\node.csv";
     private static final String objectFilePath = "C:\\Users\\afournier\\IdeaProjects\\valfilechecker\\src\\main\\resources\\objects.csv";
-    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+    private static final String outputFilePath = "C:\\Users\\afournier\\IdeaProjects\\valfilechecker\\src\\main\\resources\\output.csv";
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
 
 
     public static void main(String[] args) throws Exception {
 
 
-
-        List<Objects> objects = readObjects(objectFilePath);
+        LOGGER.error("Starting the process");
+        LOGGER.error("reading node.csv");
         Map<String, NodeCsv> nodes = readNodes(nodeFilePath);
+
+        LOGGER.error("processing node.csv");
         UnifiedNodeCollection unl = new UnifiedNodeCollection();
 
 
@@ -53,17 +63,17 @@ public class Main {
                     }
                     // store the object into collection
                     unl.put(un);
-                    System.out.println(node.toString());
-                    System.out.println("New procedure object added:" + un.toString());
+                    LOGGER.debug(node.toString());
+                    LOGGER.debug("New procedure object added:" + un.toString());
                     break;
                 case "PLSQL Block":
                     // for PL/SQL block, i.e. procedure body, mark it as having body, i.e. exportable into EDC
-                    // System.out.println("Marking PLSQL Block " + node.toString() + " with HASBODY");
+                    // LOGGER.debug("Marking PLSQL Block " + node.toString() + " with HASBODY");
                     // find parent object (procedure) by node.csv Id
-                    System.out.println("Found PLSQL Block trying to tag parent" + node.toString());
+                    LOGGER.debug("Found PLSQL Block trying to tag parent" + node.toString());
                     NodeCsv myParent = nodes.get(node.getParentNodeId());
                     if (myParent == null) {
-                        System.out.println("No parent found" + node.toString());
+                        LOGGER.debug("No parent found" + node.toString());
                         continue;
                     }
 
@@ -71,9 +81,9 @@ public class Main {
                     un = unl.getByNodeId(myParent.getNodeId());
                     if (un != null) {
                         un.setHasBody(true);
-                        System.out.println("Procedure Object tagged:" + un.toString());
+                        LOGGER.debug("Procedure Object tagged:" + un.toString());
                     } else
-                        System.out.println("Unified object not found for " + myParent.getNodeId());
+                        LOGGER.debug("Unified object not found for " + myParent.getNodeId());
 
                     break;
                 default:
@@ -84,6 +94,10 @@ public class Main {
 
 
         // step #2
+        LOGGER.error("reading objects.csv");
+        List<Objects> objects = readObjects(objectFilePath);
+
+        LOGGER.error("processing objects.csv");
         // go through Objects.csv and match all Procedures to existing Unified object to mark it as present in Objects.csv
         for (Objects object : objects) {
             // skip all objects that are not procedures
@@ -96,7 +110,7 @@ public class Main {
              */
             String identity = object.getIdentityId();
             String[] idParts = identity.split("/");
-            String fullPath = ""/*schema+package+name*/;
+            String fullPath = null;
             String objectName = null;
             String objectSchema = null;
             String objectPackage = null;
@@ -116,10 +130,11 @@ public class Main {
             // mark Unified object as resent in Objects.csv
             UnifiedNode un = unl.getByFullPath(fullPath);
             if (un != null) {
+                LOGGER.debug("Unified object  found tagging fullname" + fullPath);
                 un.setInObjectsCsv(true);
             } else {
                 //if Unified object not found, then create it.  This should never happen as all entries from objects.csv should always be in node.csv.
-                System.out.println("Unified object not found for fullname" + fullPath);
+                LOGGER.debug("Unified object not found for fullname" + fullPath);
                 un = new UnifiedNode();
                 un.setObjectSourceId(fullPath);
                 un.setObjectName(objectName);
@@ -132,10 +147,16 @@ public class Main {
 
         }
 
-        //print the result
-        unl.values().forEach(System.out::println);
+        LOGGER.error("writing output.csv");
+        csvWriterAll(unl,outputFilePath);
+
+        LOGGER.error("Completed");
+
+        /**/
+
     }
 
+    //Read Csv to Java Pojo
     public static List<Objects> readObjects(final String objectFilePath) throws Exception {
         try (
                 Reader reader = Files.newBufferedReader(Paths.get(objectFilePath))
@@ -152,7 +173,7 @@ public class Main {
         }
 
     }
-
+ // Read Csv to Java Pojo
     public static Map<String, NodeCsv> readNodes(final String nodeFilePath) throws Exception {
         try (
                 Reader reader1 = Files.newBufferedReader(Paths.get(nodeFilePath))
@@ -174,19 +195,20 @@ public class Main {
         }
 
     }
-}
+
+
+    public static void csvWriterAll(UnifiedNodeCollection unl, final String outputFilePath) throws Exception{
+        Writer writer = new FileWriter(outputFilePath);
+        StatefulBeanToCsv beanToCsv = new StatefulBeanToCsvBuilder<>(writer).build();
+
+        beanToCsv.write(unl.values());
+        beanToCsv.write(unl.values());
+        writer.close();
+        }
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
+    }
